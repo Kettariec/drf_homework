a67bdf65527646d598541101f0d332dbdeb4718d
Konstantin Senkin <kettariec@gmail.com>
2024-03-17 12:51:43 +0300

    homework 5 done .

--- study/urls.py
+++ study/urls.py
@@ -4,7 +4,8 @@ from rest_framework import routers
 from study.views import (LessonListView, LessonCreateView,
                          LessonDetailView, LessonUpdateView,
                          LessonDeleteView, CourseViewSet,
-                         SubscriptionAPIView)
+                         SubscriptionAPIView, PayCourseAPIView,
+                         CheckPaymentAPIView)
 
 app_name = StudyConfig.name
 
@@ -16,7 +17,10 @@ urlpatterns = [
     path('lesson/<int:pk>/update/', LessonUpdateView.as_view(), name='lesson_update'),
     path('lesson/<int:pk>/delete/', LessonDeleteView.as_view(), name='lesson_delete'),
 
-    path('subscription/', SubscriptionAPIView.as_view(), name='subscription')
+    path('subscription/', SubscriptionAPIView.as_view(), name='subscription'),
+
+    path('pay_course/<int:pk>/', PayCourseAPIView.as_view(), name='pay_course'),
+    path('check_pay/<int:pk>/', CheckPaymentAPIView.as_view(), name='check_pay'),
 ]
 
 router = routers.SimpleRouter()
