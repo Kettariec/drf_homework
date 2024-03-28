15def3d7e01c9edb14f5e25b81297a0cb74e8d4d
Konstantin Senkin <kettariec@gmail.com>
2024-03-28 14:05:02 +0300

    .

--- study/management/commands/send_newsletter.py
+++ study/management/commands/send_newsletter.py
@@ -2,6 +2,8 @@ import os
 import requests
 from django.core.management import BaseCommand
 
+MY_ID = os.getenv('TELEGRAM_MY_ID')
+
 
 class Command(BaseCommand):
     URL = 'https://api.telegram.org/'
@@ -10,6 +12,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         requests.post(url=f'{self.URL}bot{self.Token}/sendMessage',
-                      data={'chat_id': '493656021',
+                      data={'chat_id': MY_ID,
                             'text': 'Вы подписались на курс!'
                             },)
